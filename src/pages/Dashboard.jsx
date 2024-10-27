4d848ccc70438a08c229b2dc5f2da3109b0ac1d0
Seven0730 <hanhai0730@gmail.com>
2024-10-27 15:21:56 +0800

    QAC-83

--- src/pages/Dashboard.jsx
+++ src/pages/Dashboard.jsx
@@ -1,10 +1,16 @@
-import React, { useEffect, useState } from 'react';
+import React from 'react';
+import { jwtDecode } from 'jwt-decode'
 
 const Dashboard = () => {
+
+  const token = localStorage.getItem('jwtToken');
+  const decodedToken = jwtDecode(token);
+  const subject = decodedToken.sub;
 
   return (
     <div>
       <h1>Dashboard Page</h1>
+      <h1>Subject: {subject}</h1>
     </div>
   );
 };
