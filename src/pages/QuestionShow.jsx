dc477f8bc9baeb3db172a7cdfbe3650660b780a8
giftsshow <gggengyr@gmail.com>
2024-10-29 10:34:44 +0800

    show ownername of question

--- src/pages/QuestionShow.jsx
+++ src/pages/QuestionShow.jsx
@@ -10,7 +10,9 @@ import ThumbDownAltIcon from '@mui/icons-material/ThumbDownAlt';
 import CommentIcon from '@mui/icons-material/Comment';
 import DeleteIcon from '@mui/icons-material/Delete';
 import CommentList from './CommentList'; 
-import { jwtDecode } from 'jwt-decode'
+import { fetchUserProfile } from '../services/userService'; 
+import { jwtDecode } from 'jwt-decode';
+import PersonIcon from '@mui/icons-material/Person';
 
 const ShowQuestion = () => {
   const { id } = useParams();
@@ -22,22 +24,31 @@ const ShowQuestion = () => {
   const [commentPanels, setCommentPanels] = useState({});
   const [comments, setComments] = useState({});
   const [newComment, setNewComment] = useState(''); 
-  const [upvotes, setUpvotes] = useState({}); // upvote
-  const decodeToken = jwtDecode(localStorage.getItem('jwtToken'))
-  const userId = decodeToken.sub; 
+  const [upvotes, setUpvotes] = useState({}); 
+  const [username, setUsername] = useState(''); 
+
+  const decodeToken = jwtDecode(localStorage.getItem('jwtToken'));
+  const userId = decodeToken.sub;
 
   useEffect(() => {
     const getQuestionAndAnswers = async () => {
       try {
         const questionData = await fetchQuestionById(id);
         setQuestion(questionData);
+
+
+        if (questionData.ownerId) {
+          const userProfile = await fetchUserProfile(questionData.ownerId);
+          setUsername(userProfile.username);
+        }
+
         const answerData = await fetchAnswersByQuestionId(id);
         setAnswers(answerData);
 
         const initialCommentPanels = {};
-        answerData.forEach(answer => {
+        answerData.forEach((answer) => {
           initialCommentPanels[answer.id] = false;
-          fetchUpvoteCount(answer.id); // get the initial number of upvotes
+          fetchUpvoteCount(answer.id); 
         });
         setCommentPanels(initialCommentPanels);
       } catch (err) {
@@ -49,7 +60,7 @@ const ShowQuestion = () => {
     getQuestionAndAnswers();
   }, [id]);
 
-  // get the number of upvotes
+
   const fetchUpvoteCount = async (answerId) => {
     try {
       const count = await getUpvoteCount(answerId);
@@ -62,7 +73,6 @@ const ShowQuestion = () => {
     }
   };
 
-  // handle upvote click events
   const handleUpvoteClick = async (answerId, ownerId) => {
     try {
       await clickUpvote({ userId: userId, postId: answerId, authorId: ownerId, authorEmail: 'no.need', postType: 'ANSWER' });
@@ -74,7 +84,7 @@ const ShowQuestion = () => {
 
   const handleCreateAnswer = async () => {
     try {
-      const answerData = { content: newAnswer, questionId: id, ownerId: userId};
+      const answerData = { content: newAnswer, questionId: id, ownerId: userId };
       const createdAnswer = await createAnswer(answerData);
       setAnswers([...answers, createdAnswer]);
       setNewAnswer('');
@@ -86,26 +96,22 @@ const ShowQuestion = () => {
 
   const toggleCommentPanel = async (answerId) => {
     setCommentPanels((prevPanels) => ({
-        ...prevPanels,
-        [answerId]: !prevPanels[answerId],
+      ...prevPanels,
+      [answerId]: !prevPanels[answerId],
     }));
 
     if (!commentPanels[answerId] && !comments[answerId]) {
-        try {
-            const fetchedComments = await fetchCommentsByAnswerId(answerId);
-            setComments((prevComments) => ({
-                ...prevComments,
-                [answerId]: fetchedComments,
-            }));
-
-            console.log('Fetched comments for answer ID:', answerId);
-            console.log('Current comments state:', { ...comments, [answerId]: fetchedComments }); 
-
-        } catch (error) {
-            console.error('Failed to fetch comments:', error);
-        }
+      try {
+        const fetchedComments = await fetchCommentsByAnswerId(answerId);
+        setComments((prevComments) => ({
+          ...prevComments,
+          [answerId]: fetchedComments,
+        }));
+      } catch (error) {
+        console.error('Failed to fetch comments:', error);
+      }
     }
-};
+  };
 
 
   const handleDeleteAnswer = async (answerId) => {
@@ -118,28 +124,27 @@ const ShowQuestion = () => {
     }
   };
 
+
   const handleCommentChange = (e) => {
     setNewComment(e.target.value);
   };
   
+
   const handleCommentSubmit = async (answerId) => {
     try {
-        const createdComment = await sendComment(userId, answerId, newComment);
-
-        setComments((prevComments) => ({
-            ...prevComments,
-            [answerId]: [
-                ...(prevComments[answerId] || []),
-                createdComment, 
-            ],
-        }));
-
-        setNewComment(''); 
+      const createdComment = await sendComment(userId, answerId, newComment);
+      setComments((prevComments) => ({
+        ...prevComments,
+        [answerId]: [
+          ...(prevComments[answerId] || []),
+          createdComment, 
+        ],
+      }));
+      setNewComment(''); 
     } catch (error) {
-        console.error('Failed to send comment:', error);
+      console.error('Failed to send comment:', error);
     }
   };
-
 
   if (loading) {
     return <CircularProgress />;
@@ -154,11 +159,22 @@ const ShowQuestion = () => {
       <Typography variant="h4" gutterBottom align="center" style={{ fontWeight: 'bold' }}>
         {question.title}
       </Typography>
+      <div style={{ display: 'flex', justifyContent: 'center', alignItems: 'center', marginBottom: '16px' }}>
+        <PersonIcon style={{ color: '#3f51b5', marginRight: '8px' }} />
+        <Typography
+          variant="h6"
+          align="center"
+          style={{ fontWeight: 'bold', color: '#3f51b5' }} 
+        >
+          {username}
+        </Typography>
+      </div>
       <Divider style={{ margin: '20px 0' }} />
       <Typography variant="body1" style={{ lineHeight: '1.8', fontSize: '1.2rem', textAlign: 'justify' }}>
         {question.content}
       </Typography>
       <Divider style={{ margin: '20px 0' }} />
+
       <TextField
         label="Your Answer"
         value={newAnswer}
@@ -171,6 +187,7 @@ const ShowQuestion = () => {
         Submit Answer
       </Button>
       <Divider style={{ margin: '20px 0' }} />
+
       {answers.map((answer) => (
         <Paper key={answer.id} style={{ padding: '16px', marginBottom: '10px', position: 'relative' }}>
           <Typography variant="body2">{answer.content}</Typography>
@@ -205,7 +222,6 @@ const ShowQuestion = () => {
 
           {commentPanels[answer.id] && (
             <div style={{ marginTop: '16px', padding: '16px', border: '1px solid #ddd', borderRadius: '8px' }}>
-              
               {/* Input Comment */}
               <div style={{ display: 'flex', flexDirection: 'column', alignItems: 'flex-start', marginBottom: '10px', width: '100%' }}>
                 <div style={{ display: 'flex', alignItems: 'center', width: '100%' }}>
@@ -224,15 +240,14 @@ const ShowQuestion = () => {
                 </div>
 
                 <Button
-                    variant="contained"
-                    color="primary"
-                    disabled={!newComment}
-                    style={{ marginTop: '8px', marginLeft: '50px' }}
-                    onClick={() => handleCommentSubmit(answer.id)} 
+                  variant="contained"
+                  color="primary"
+                  disabled={!newComment}
+                  style={{ marginTop: '8px', marginLeft: '50px' }}
+                  onClick={() => handleCommentSubmit(answer.id)} 
                 >
-                    Submit Comment
+                  Submit Comment
                 </Button>
-
               </div>
 
               <CommentList comments={comments[answer.id] || []} />
